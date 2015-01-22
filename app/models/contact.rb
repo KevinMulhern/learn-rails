837d960e823f2e6211b63c60ad08300b19440440
Kevin Mulhern <kevinmulhern23@gmail.com>
2015-01-22 15:24:15 +0000

    Sending mail

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,4 @@
 require "google_drive_v0"
-
 class Contact
 	include ActiveModel::Model 
 	attr_accessor :name, :string
@@ -15,6 +14,7 @@ class Contact
 
 	def update_spreadsheet
 		connection = GoogleDriveV0.login(Rails.application.secrets.email_provider_username, Rails.application.secrets.email_provider_password)
+		
 		ss = connection.spreadsheet_by_title('Learn-Rails-Example')
 		if ss.nil?
 			ss = connection.create_spreadsheet('Learn-Rails-Example')
